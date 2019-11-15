fd9b8b94605eeaad5c1444eefbb52501890af4a9
acro-oogi <oogi@acroquest.co.jp>
2019-11-15 10:29:55 +0900

    Apply Elastic APM.

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -4,10 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+import co.elastic.apm.attach.ElasticApmAttacher;
+
 @SpringBootApplication
 public class PetClinicApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
+		ElasticApmAttacher.attach();
 		SpringApplication.run(PetClinicApplication.class, args);
 	}
 }
